f9804c4e19ca6f948ec884a682448c2757d41c2d
Slaxzer96 <peter@qrholm.dk>
2019-04-03 14:50:32 +0200

    adding numpad

--- emp-rtcs/modules/numpad.c
+++ emp-rtcs/modules/numpad.c
@@ -1,4 +1,4 @@
-/****************************************************************************
+data_row/****************************************************************************
 * University of Southern Denmark
 * RB-PRO4 F19
 *
@@ -14,6 +14,7 @@
 
 #include "numpad.h"
 #include "tp.h"
+#include "memset"
 
 /*****************************    Defines    *******************************/
 
@@ -52,11 +53,11 @@ void NUMPAD_init(void (*callback)(uint8_t byte))
 
 	// PORTA and PortC Direction
 	GPIO_PORTA_DIR_R	&= ~( ( 1 << 2 ) | ( 1 << 3 ) | ( 1 << 4 ) );
-	GPIO_PORTE_DIR_R	&= ~( ( 1 << 0 ) | ( 1 << 1 ) | ( 1 << 2 ) | ( 1 << 3 ) );
+	GPIO_PORTE_DIR_R	|= ( ( 1 << 0 ) | ( 1 << 1 ) | ( 1 << 2 ) | ( 1 << 3 ) );
 
     //
     GPIO_PORTA_PUR_R    |= ( ( 1 << 2 ) | ( 1 << 3 ) | ( 1 << 4 ) );
-    GPIO_PORTE_PUR_R    |= ( ( 1 << 0 ) | ( 1 << 1 ) | ( 1 << 2 ) | ( 1 << 3 ) );
+    //GPIO_PORTE_PUR_R    |= ( ( 1 << 0 ) | ( 1 << 1 ) | ( 1 << 2 ) | ( 1 << 3 ) );
 
 	// PortC and PortD Digital
 	GPIO_PORTA_DEN_R	|= ( ( 1 << 2 ) | ( 1 << 3 ) | ( 1 << 4 ) );
@@ -70,36 +71,52 @@ void NUMPAD_operate()
 	static TIMEPOINT* tp_timer = NULL;
 	if (tp_timer == NULL) { tp.new(); }
 
-	uint32_t  column = GPIO_PORTA_DATA_R;
-	uint32_t  row = GPIO_PORTE_DATA_R;
+	uint32_t  data_col = GPIO_PORTA_DATA_R;
+	uint32_t  data_row = GPIO_PORTE_DATA_R;
 
-	static uint32_t col_row[3][4];
+	static uint32_t shift_reg[12];
 
-	// for(int row_ = 0; row_ < 4; row_++)
-	// {
-	// 	for(int col_ = 0; col_ < 3; col_++)
-	// 	{
-	// 		//col_row[col_][row_] |= ( ((column >> 2 + col_) & 1 )  &( (row >> row_)) & 1));
-	// 		col_row[col_][row_] <<= 1;
-	// 		// shift AFTER masking???
-	//
-	// 		if(!col_row[col_][row_])
-	// 		{
-	// 			if(200 >tp.delta_now(timer,ms))
-	// 			{
-	// 				tp.set(tp_timer, tp.now());
-	// 				numpad.callback('1');
-	// 				col_row[col_][row_] = 0xffffffff;
-	// 			}
-	// 		}
-	// 	}
-	// }
+	 for(int row_idx = 0; row_idx < 4; row_idx++)
+	 {
+		 data_row &= ~( 1 << 0 ) | ( 1 << 1 ) | ( 1 << 2 ) | ( 1 << 3 ));
+		 data_row |= (1 << row_idx);
+
+	 	for(int col_idx = 1; col_idx < 4; col_idx++)
+	 	{
+			shift_reg[col_idx + row_idx * 3] <<= 1;
+			shift_reg[col_idx + row_idx * 3] |= ( ( (data_col >> 2 + col_idx -1 ) & 1 ) );
+
+	 		if( (!(shift_reg[col_idx + row_idx *3 ]) && tp.delta_now(tp_timer,ms) > 200) )
+	 		{
+ 				tp.set(tp_timer, tp.now());
+				char tmp = _NUMPAD_getchar(row_idx ,col_idx);
+ 				numpad.callback(tmp);
+				memset(&shift_reg, 0, 11);
+	 		}
+	 	}
+	 }
 }
 
 
 
-void NUMPAD_del()
+static uint8_t 	_NUMPAD_getchar(uint8_t row, uint8_t col)
 {
+	if(row*3 + col >9)
+	{
+		switch(row*3 + col)
+		{
+			case 10:
+				return '*';
 
+			case 11:
+				return '0';
 
+			case 12:
+				return '#';
+		}
+	}
+	else
+	{
+		return '0'+ row*3 + col;
+	}
 }
